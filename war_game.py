d93589e90783985938dd4ef703382512b2c4d086
Aditya Sutar <adityasutar511@gmail.com>
2025-09-01 01:01:26 +0530

    WCG-8 Fixed Infinite Loop Bug

--- war_game.py
+++ war_game.py
@@ -11,8 +11,9 @@ player1 = deck[:26]
 player2 = deck[26:]
 
 round_num = 0
+# if game doesnt end in 10K rounds, declare winner based on number of cards
 
-while player1 and player2:  # while both players have cards
+while ((player1 and player2) and (round_num<10000)):  # while both players have cards
     round_num += 1
     card1 = player1.pop(0)   # take top card
     card2 = player2.pop(0)
@@ -29,10 +30,15 @@ while player1 and player2:  # while both players have cards
         print("Tie! Both cards discarded\n")
         # do nothing (cards are discarded)
 
+    print(f"Player 1 has {len(player1)} cards, Player 2 has {len(player2)} cards\n")
+
+
 # Determine winner
-if player1:
+if len(player1) > len(player2):
     print("🎉 Player 1 wins the game!")
-else:
+elif len(player2) > len(player1):
     print("🎉 Player 2 wins the game!")
+else:
+    print("It's a draw!")
         # do nothing (cards are discarded)
 
